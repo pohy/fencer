87785225bf74aff5eb936049ec8517d2b9d0ecf6
pohy <0ronon0@gmail.com>
2024-01-23 14:46:44 +0100

    Improve brush positioning when not hovering over a plank

--- src/brush_caster.gd
+++ src/brush_caster.gd
@@ -1,6 +1,6 @@
 extends Node3D
 
-@export var speed: float = 1.0
+@export var brush_speed: float = 1.0
 @export var depletion_rate: float = 1.0
 @export var apply_interval_s: float = 0.1
 @export var apply_velocity_multiplier: float = 1.0
@@ -11,8 +11,8 @@ extends Node3D
 @onready var _brush_fill_progress: ProgressBar = $BrushModel/SubViewport/ProgressBar
 
 
-var _target_position: Vector2 = Vector2.ZERO
-var _current_position: Vector2 = Vector2.ZERO
+var _target_cursor_position: Vector2 = Vector2.ZERO
+var _current_cursor_position: Vector2 = Vector2.ZERO
 var _paint_fill: float = 1.0
 var _distance_travelled: float = 0.0
 var _since_last_application_s: float = 0.0
@@ -20,15 +20,16 @@ var _brush_initial_basis: Basis = Basis.IDENTITY
 var _is_above_plank: bool = false
 var _mouse_position_3d: Vector3 = Vector3.ZERO
 var _current_plank: Plank = null
+var _target_brush_position: Vector3 = Vector3.ZERO
 
 func _ready():
 	_brush_initial_basis = Basis(_brush_model.basis)
 
 func _process(delta):
-	_target_position = _mouse.position
-	var move_delta = _current_position - _target_position
+	_target_cursor_position = _mouse.position
+	var move_delta = _current_cursor_position - _target_cursor_position
 	var velocity = move_delta.length()
-	_current_position -= move_delta
+	_current_cursor_position -= move_delta
 	_distance_travelled += velocity
 
 	_apply_tilt(delta, move_delta, velocity)
@@ -38,26 +39,39 @@ func _process(delta):
 	if _mouse.left_released:
 		_paint_fill = 1.0
 
-	_update_brush_position()
+	_update_brush_position(delta)
 	_apply_brush_stroke(delta, move_delta, velocity)
 
 	_brush_fill_progress.value = _paint_fill
 
-func _update_brush_position():
+func _update_brush_position(delta: float):
 	var camera := get_viewport().get_camera_3d()
-	var from := camera.project_ray_origin(_current_position)
-	var to := from + camera.project_ray_normal(_current_position) * 1000
-	var query_params := PhysicsRayQueryParameters3D.create(from, to)
+	var origin := camera.project_ray_origin(_current_cursor_position)
+	var direction := camera.project_ray_normal(_current_cursor_position)
+	var to := origin + direction * 1000
+	var query_params := PhysicsRayQueryParameters3D.create(origin, to)
 	var query := get_world_3d().direct_space_state.intersect_ray(query_params)
 
-	if query.size() <= 0:
-		# TODO: Move brush to a "resting" position
-		return
-
+	# if query.size() <= 0:
+	# 	# _brush_model.global_position = origin + direction * 1.5
+	# 	_target_brush_position = origin + direction
 	_is_above_plank = query.size() > 0 and query.collider is Plank
-	_mouse_position_3d = query.position
-	_brush_model.global_position = query.position + query.normal * 0.1
-	_current_plank = query.collider if _is_above_plank else null
+
+	if _is_above_plank:
+		_mouse_position_3d = query.position
+		# _brush_model.global_position = query.position + query.normal * 0.1
+		var distance_from_plank = 0.05 if _mouse.left else 0.18
+		_target_brush_position = query.position + query.normal * distance_from_plank
+		_current_plank = query.collider# if _is_above_plank else null
+
+	if not _is_above_plank and not _mouse.left:
+		# print_debug("nulling current plank")
+		_current_plank = null
+		_target_brush_position = origin + direction
+
+	# print_debug("Target brush position: %s, query: %s" % [_target_brush_position, query])
+
+	_brush_model.global_position = lerp(_brush_model.global_position, _target_brush_position, brush_speed * delta)
 	
 
 func _apply_brush_stroke(delta: float, move_delta: Vector2, velocity: float):

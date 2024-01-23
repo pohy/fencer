9910765bf6e6b7e34ef58f8c0014d6b2b4198e62
pohy <0ronon0@gmail.com>
2024-01-23 20:52:41 +0100

    Add paint fill progress tracking

---
+++ src/paint_progress_tracker.gd
@@ -0,0 +1,24 @@
+extends Node
+
+signal updated(progress: float)
+
+var paint_fill_progress: float:
+    get:
+        return _fill_sum / _planks.size()
+
+var _fill_sum: float = 0.0
+var _planks: Array[Plank] = []
+
+func _ready():
+    var maybe_planks = get_tree().get_nodes_in_group("plank")
+    for plank in maybe_planks:
+        if not plank is Plank:
+            continue
+        
+        _planks.append(plank)
+        plank.fill_updated.connect(_on_fill_updated)
+
+
+func _on_fill_updated(fill_delta: float):
+    _fill_sum += fill_delta
+    updated.emit(paint_fill_progress)
--- src/plank/plank.gd
+++ src/plank/plank.gd
@@ -1,10 +1,45 @@
 class_name Plank
 extends StaticBody3D
 
+signal fill_updated(fill_delta: float)
+
+@export var fill_interval_s: float = 1.0
+
+var fill_amount: float:
+	get:
+		return _current_fill
+
 @onready var _sub_viewport: Viewport = $SubViewport
 @onready var _brush: Sprite2D = $SubViewport/BrushSprite
 @onready var _mesh: MeshInstance3D = $MeshInstance3D
+@onready var _inactive_timer: Timer = $InactiveTimer
 
+var _current_fill: float = 0.0
+var _last_fill_fetch_at := -1.0
+
+func _ready():
+	_inactive_timer.timeout.connect(_update_fill_amount)
+
+func _update_fill_amount():
+	_last_fill_fetch_at = Time.get_ticks_msec()
+	var fill = fetch_fill_amount()
+	var fill_delta = fill - _current_fill
+	_current_fill = fill
+	fill_updated.emit(fill_delta)
+
+# Returns normalized fill amount (in 0 -> 1 range)
+func fetch_fill_amount() -> float:
+	var image := _sub_viewport.get_texture().get_image()
+	var w := image.get_width()
+	var h := image.get_height()
+	var full_fill := w * h
+	var current_fill := 0.0
+	for i in range(0, full_fill):
+		var pos := Vector2i(i % w, i / w)
+		var pixel := image.get_pixelv(pos)
+		current_fill += pixel.a
+
+	return current_fill / full_fill
 
 # TODO: Accept modulation color
 func stroke_brush(cursor_position_3d: Vector3, opacity: float = 1.0, rotation: float = 0.0, scale: float = 1.0):
@@ -14,6 +49,12 @@ func stroke_brush(cursor_position_3d: Vector3, opacity: float = 1.0, rotation: f
 	_brush.position = viewport_mouse_pos
 	_brush.modulate.a = opacity
 	_brush.rotation_degrees = rotation
+
+	if Time.get_ticks_msec() - _last_fill_fetch_at > fill_interval_s * 1000:
+		_update_fill_amount()
+		_inactive_timer.stop()
+	else:
+		_inactive_timer.start()
 
 func _calculate_viewport_mouse_position(mouse_position_3d: Vector3) -> Vector2:
 	var mouse_pos_3d := global_transform.affine_inverse() * mouse_position_3d
